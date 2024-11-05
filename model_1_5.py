1fe42f55a7fa568fe7a647d52a2908336c6af727
Alina Malofeeva <alina0203181@gmail.com>
2024-11-05 14:00:34 +0300

    Домашнее задание

---
+++ model_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = (1, 2, 3, 4, 'a', 'ban')
+print(immutable_var)
+mutable_list = [2004, 2005, 2006, 'apple']
+print(mutable_list)
+mutable_list [1] = 2003
+print(mutable_list)
+mutable_list.append(True)
+print(mutable_list)
+mutable_list.remove(2006)
+print(mutable_list)
